d2ea03c8a52e80238ec2bb9bc2b1467a7f113c4a
idawnlight <idawn@live.com>
2026-05-02 13:51:09 +0900

    fix: add --clamp-mss-to-pmtu for better forward performance

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,13 @@ sleep 1
 echo "Starting warp-svc..."
 warp-svc &
 
-# Give the daemon a few seconds to initialize
-sleep 8
-
 # Set up NAT Masquerading for the LAN gateway feature
 echo "Configuring iptables NAT rule..."
 iptables -t nat -A POSTROUTING -o CloudflareWARP -j MASQUERADE
+
+# Force TCP packets to adapt to the WARP MTU to prevent fragmentation
+echo "Applying TCP MSS Clamping..."
+iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 echo "Starting gost..."
 gost -L "http://:1080?udp=true" -L "socks5://:1081?udp=true" &
